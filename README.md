a4971eec77152f04d99168b3e691f8f1a5f30de7
samuel <samuelcontatodev@gmail.com>
2022-02-15 10:56:35 -0300

    recuperação de senha como nodemailer gerando token

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 Usando ferramenta mailtrap (basicamente uma caixa de email fake) - essa <br> 
 ferramenta de email personalizada é recomendada para testes, em produção usamos <br> 
-outros métodos para entrega de email transacional, como: sparkpost, mailchimp, sendgrid
+outros métodos para entrega de email transacional, como: sparkpost, mailchimp, sendgrid <br><br>
+
+Usando o pacote nodemailer-express-handlebars como template de email html <br><br>
+
--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const authConfig = require('../../config/auth.json')
+const crypto = require('crypto')
+const mailer = require('../../modules/mailer')
 
 const User = require('../models/user')
 
@@ -31,7 +33,7 @@ router.post('/register', async (req, res) => {
             user,
             token: generateToken({ id: user.id })
         })
-        
+
     } catch (err) {
         return res.status(400).send({ error: 'Registration failed' })
     }
@@ -60,6 +62,81 @@ router.post('/authenticate', async (req, res) => {
         user,
         token: generateToken({ id: user.id })
     })
+})
+
+router.post('/forgot_password', async (req, res) => {
+    const { email } = req.body
+
+    try {
+
+        //verificando se email está cadastrado na base de user
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return res.status(400).send({ error: 'User not found' })
+        }
+
+        const token = crypto.randomBytes(20).toString('hex')
+
+        const now = new Date()
+        now.setHours(now.getHours() + 1)
+
+        await User.findByIdAndUpdate(user.id, {
+            '$set': {
+                passwordResetToken: token,
+                passwordResetExpires: now,
+            }
+        })
+
+        mailer.sendMail({
+            to: email,
+            from: 'diego@gmail.com',
+            template: 'auth/forgot_password',
+            context: { token },
+        }, (err) => {
+
+            if (err) {
+                return res.status(400).send({ error: 'Cannot send forgot password email' })
+            }
+
+            return res.send()
+        })
+
+    } catch (error) {
+        res.status(400).send({ error: 'Erro on forgot password, try again' })
+    }
+})
+
+router.post('/reset_password', async (req, res) => {
+    const { email, token, password } = req.body
+
+    try {
+        const user = await User.findOne({ email })
+            .select('+passwordResetToken passwordResetExpires')
+
+        if (!user) {
+            return res.status(400).send({ error: 'User not found' })
+        }
+
+        if (token !== user.passwordResetToken) {
+            return res.status(400).send({ error: 'Token invalid'})
+        }
+
+        const now = new Date ()
+
+        if(now > user.passwordResetExpires) {
+            return res.status(400).send({ error: 'Token expired, generate a new one'})
+        }
+
+        user.password = password
+
+        await user.save()
+        
+        res.send()
+
+    } catch (error) {
+        res.status(400).send({ error: 'Cannot reset password, try again' })
+    }
 })
 
 module.exports = app => app.use('/auth', router)
