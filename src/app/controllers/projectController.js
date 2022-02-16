f25d5b2fc4f4d5d7b83c20a0503bee7383566ce5
samuel <samuelcontatodev@gmail.com>
2022-02-16 10:03:46 -0300

    apredendo a fazer crud no mongo, projeto finalizado

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -10,7 +10,7 @@ router.use(authMiddleware)
 
 router.get('/', async (req, res) => {
     try {
-        const projects = await Project.find()
+        const projects = await Project.find().populate(['user', 'tasks'])
 
         return res.send({ projects })
     } catch (error) {
@@ -18,26 +18,75 @@ router.get('/', async (req, res) => {
     }
 })
 
-router.get('/:projectId', (req, res) => {
-    res.send({ user: req.userId })
+router.get('/:projectId', async (req, res) => {
+    try {
+        const project = await Project.findById(req.params.projectId).populate(['user', 'tasks'])
+
+        return res.send({ project })
+    } catch (error) {
+        return res.status(400).send({ error: 'Error loading project' })
+    }
 })
 
 router.post('/', async (req, res) => {
     try {
-        const project = await Project.create({ ...req.body, user: req.userId })
+        const { title, description, tasks } = req.body
+
+        const project = await Project.create({ title, description, user: req.userId })
+
+        await Promise.all(tasks.map(async task => {
+            const projectTask = new Task({ ...task, project: project._id })
+
+            await projectTask.save()
+            project.tasks.push(projectTask)
+        }))
+
+        await project.save()
 
         return res.send({ project })
+
     } catch (error) {
         return res.status(400).send({ error: 'Error creating new project' })
     }
 })
 
-router.put('/:projectId', (req, res) => {
-    res.send({ user: req.userId })
+router.put('/:projectId', async (req, res) => {
+    try {
+        const { title, description, tasks } = req.body
+
+        const project = await Project.findByIdAndUpdate(req.params.projectId, {
+            title,
+            description,
+        }, { new: true })//retorna o projeto atualizado, por padrão o mongo trás o projeto antigo
+
+        //deletando as antigas antes de cadastrar as novas
+        project.tasks = []
+        await Task.remove({ project: project._id })
+
+        await Promise.all(tasks.map(async task => {
+            const projectTask = new Task({ ...task, project: project._id })
+
+            await projectTask.save()
+            project.tasks.push(projectTask)
+        }))
+
+        await project.save()
+
+        return res.send({ project })
+
+    } catch (error) {
+        return res.status(400).send({ error: 'Error updating project' })
+    }
 })
 
-router.delete('/:projectId', (req, res) => {
-    res.send({ user: req.userId })
+router.delete('/:projectId', async (req, res) => {
+    try {
+        await Project.findByIdAndRemove(req.params.projectId)
+
+        return res.send()
+    } catch (error) {
+        return res.status(400).send({ error: 'Error deleting project' })
+    }
 })
 
 router.get('/:projectId')
