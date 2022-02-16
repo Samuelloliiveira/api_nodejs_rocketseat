d33c6db34848c36c0e8a3858de1ff253ddce0f97
samuel <samuelcontatodev@gmail.com>
2022-02-15 22:57:21 -0300

    criando crud de projetos e tarefas

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # api_nodejs_rocketseat <br>
 
+Nessa api registramos usuários no banco MongoDB <br>
+esse usuário pode autenticar e caso sua senha estiver errada <br>
+ele pode recuperar seu acesso através de um token enviado pelo email. <br><br>
+
+Na api fizemos CRUD, onde o usuário pode cadastrar, atualizar, <br> pesquisar, e deletar projetos e tarefas
+
 Usando ferramenta mailtrap (basicamente uma caixa de email fake) - essa <br> 
 ferramenta de email personalizada é recomendada para testes, em produção usamos <br> 
 outros métodos para entrega de email transacional, como: sparkpost, mailchimp, sendgrid <br><br>
--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,12 +1,45 @@
 const express = require('express')
 const authMiddleware = require('../middlewares/auth')
 
+const Project = require('../models/project')
+const Task = require('../models/task')
+
 const router = express.Router()
 
 router.use(authMiddleware)
 
-router.get('/', (req, res) => {
-    res.send({ ok: true, user: req.userId })
+router.get('/', async (req, res) => {
+    try {
+        const projects = await Project.find()
+
+        return res.send({ projects })
+    } catch (error) {
+        return res.status(400).send({ error: 'Error loading projects' })
+    }
 })
+
+router.get('/:projectId', (req, res) => {
+    res.send({ user: req.userId })
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const project = await Project.create({ ...req.body, user: req.userId })
+
+        return res.send({ project })
+    } catch (error) {
+        return res.status(400).send({ error: 'Error creating new project' })
+    }
+})
+
+router.put('/:projectId', (req, res) => {
+    res.send({ user: req.userId })
+})
+
+router.delete('/:projectId', (req, res) => {
+    res.send({ user: req.userId })
+})
+
+router.get('/:projectId')
 
 module.exports = app => app.use('/projects', router)
